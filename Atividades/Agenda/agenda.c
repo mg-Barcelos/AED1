0f407cfcfa66dfaf77af898444636c6d2347777f
Miguel Barcelos <mgbarcelosprog@gmail.com>
2025-01-22 22:05:41 -0300

    Update agenda.c

--- Atividades/Agenda/agenda.c
+++ Atividades/Agenda/agenda.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void* menu(void* pBuffer);
+void* criar_no(void* pBuffer);
+void* adicionar(void* pBuffer);
+void* remover(void* pBuffer);
+void* buscar(void* pBuffer);
+void* listar(void* pBuffer);
+void liberar_lista(void* pBuffer);
+
+int main(){
+  void* pBuffer = malloc (3* sizeof(void*) + 80);
+  *(void**) pBuffer = NULL;
+  *(void**)(pBuffer
+  *(void**)
