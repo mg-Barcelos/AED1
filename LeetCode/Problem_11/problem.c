f844ff9bb442e629a6a9501b6844fd9728a5f92c
Miguel Barcelos <mgbarcelosprog@gmail.com>
2025-03-28 13:31:33 -0300

    Atualizar o problem.c

--- LeetCode/Problem_11/problem.c
+++ LeetCode/Problem_11/problem.c
@@ -1,24 +1,4 @@
-//2558
 
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-long long pickGifts(int* gifts, int giftsSize, int k) {
-    for (int i; i < k; i++) {
-
-        int Maxgif = 0;
-        for (int j = 1; j < giftsSize; j++) {
-            if (gifts[j] > gifts[Maxgif]) {
-                Maxgif = j;
-            }
-        }
-        gifts[Maxgif] = (int)floor(sqrt(gifts[Maxgif]));
-    }
-
-    // Calcular a soma dos presentes restantes
-    long long sum = 0;
-    for (int i = 0; i < giftsSize; i++) {
         sum += gifts[i];
     }
 
